75eb28c42d55fd6abb99deda676f7ded496e7245
spijls <spijls@hotmail.com>
2021-11-21 13:59:01 +0100

    3 files modified, 6 added

--- src/application/services/todo/todo.service.impl.ts
+++ src/application/services/todo/todo.service.impl.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TodoService, TodoRepository } from '../../../application';
 import { TodoModel } from '../../models';
 
+@Injectable()
 export class TodoServiceImpl implements TodoService {
   constructor(private todoRepository: TodoRepository) {}
 
--- src/presentation/app.module.ts
+++ src/presentation/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { TodoService, TodoServiceImpl, TodoRepository } from '../application';
+import { TodoRepositoryImpl } from '../infrastructure';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TodoComponent } from './components';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [
+    { provide: TodoService, useClass: TodoServiceImpl },
+    { provide: TodoRepository, useClass: TodoRepositoryImpl },
+  ],
+  declarations: [AppComponent, TodoComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
